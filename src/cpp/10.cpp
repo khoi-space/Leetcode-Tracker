2d73499137c12abc0429c77c4f716683a0de0b87
khoinguyen <160142277+khoi-workplace@users.noreply.github.com>
2025-12-16 22:24:05 +0700

    feat: #10

--- src/cpp/10.cpp
+++ src/cpp/10.cpp
@@ -2,6 +2,10 @@
  #include "global.h"
 using namespace std;
 
+// #define DEBUG
+
+int approach = 0;
+
 /**
  * Problem 10: Regular Expression Matching
  * @input: string s and pattern p
@@ -11,10 +15,52 @@ using namespace std;
  * @output: boolean expression
  */
 bool isMatch(string s, string p) {
-    
+    if (approach == 1) {
+        // Approach 1: Recursion
+        if (p.empty()) return s.empty();
+
+        bool first_match = (!s.empty() && (s[0] == p[0] || p[0] == '.'));
+        if (p.size() >= 2 && p[1] == '*') {
+            return  isMatch(s, p.substr(2)) ||
+                    (first_match && isMatch(s.substr(1), p));
+        } else {
+            return first_match && isMatch(s.substr(1), p.substr(1));
+        }
+    } else if (approach == 2) {
+        // Approach 2: Dynamic Programming
+        int m = s.size();
+        int n = p.size();
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+        dp[m][n] = true;
+        for (int i = m; i >= 0; --i) {
+            #ifdef DEBUG
+            cout << "i=" << i << endl;
+            #endif
+
+            for (int j = n-1; j >= 0; --j) {
+                bool first_match = (i < m && (s[i] == p[j] || p[j] == '.'));
+                if (j+1 < n && p[j+1] == '*') {
+                    dp[i][j] = dp[i][j+2] || (first_match && dp[i+1][j]);
+                } else {
+                    dp[i][j] = first_match && dp[i+1][j+1];
+                }
+
+                #ifdef DEBUG
+                cout << " dp[i][j]=" << dp[i] << endl;
+                #endif
+            }
+        }
+        return dp[0][0];
+    }
+    return false;
 }
 
 void test10() {
+    cout << "Approach::\n";
+    cout << "1. Recursion\n";
+    cout << "2. Dynamic Programming\n";
+    cout << ">>> "; cin >> approach;
+
     struct Case {
         string s;
         string p;
